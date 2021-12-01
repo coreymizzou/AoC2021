a7fb5f09d97b32c4167a280381a360d4837d0077
Corey Hughes <50215735+coreymizzou@users.noreply.github.com>
2021-12-01 10:28:15 -0600

    Update day1.py

--- day1.py
+++ day1.py
@@ -1,12 +1,13 @@
 part1 = 0
 part2 = 0
 
+#put input file of numbers into list
 f = open('input.txt')
 data = f.read().splitlines()
 data = [int(i) for i in data]
 window = []
 
-#part 1
+#part 1 checks if next item in list is greater than current item
 for i in range(len(data)-1):
     if(data[i+1] > data[i]):
         part1 = part1 + 1
@@ -14,7 +15,7 @@ print(part1)
         
 
 
-#part 2
+#part 2 checks if sum of 3 consecutive items in list are greater than previous sum of 3 items
 for i in range(len(data)-2):
     window.append([data[i] + data[i+1] + data[i+2]])
     
